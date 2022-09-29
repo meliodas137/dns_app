dad64ba8ec8c7d56ac69a5fa2d3a631148e27f4b
meliodas137 <mayankgt1043@gmail.com>
2022-09-29 12:50:15 -0400

    moved code to correct file names

--- AS/server.py
+++ AS/server.py
@@ -1,18 +1,5 @@
 from flask import Flask
+import socket
 app = Flask(__name__)
 
-@app.route('/fibonacci', methods=['GET'])
-def get_params():
-    args = request.args
-    hostname = args.get('hostname')
-    fs_port = args.get('fs_port')
-    number = args.get('number')
-    as_ip = args.get('as_ip')
-    as_port = args.get('as_port')
-
-    if not args or not hostname or not fs_port or not number or not as_ip or not as_port 
-        abort(400)
-    
-    return 
-
-app.run(host='0.0.0.0', port=8080, debug=True)
+UDP_PORT = 53533
